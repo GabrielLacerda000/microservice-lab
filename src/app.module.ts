c1bfc74fc2fe60709199dc93e738cae24ea5877d
GabrielLacerda000 <gabrielglacerda000@gmail.com>
2026-04-29 12:51:24 -0300

    feat: user module added

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { MessagingModule } from './infrastructure/messaging/messaging.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { MessagingModule } from './infrastructure/messaging/messaging.module';
       isGlobal: true,
     }),
     MessagingModule,
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
